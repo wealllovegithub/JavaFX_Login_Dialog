51c7c6758cc92a8c2bdccd2f65bda74fd2212686
doughepi <doughertypiper@gmail.com>
2015-12-24 16:30:17 -0500

    Formatting.

--- sample_table.sql
+++ sample_table.sql
@@ -1,8 +1,8 @@
 CREATE TABLE user_data
 (
-  id       INT PRIMARY KEY AUTO_INCREMENT,
-  username TEXT,
-  password BINARY,
-  salt     BINARY
+  id              INT PRIMARY KEY AUTO_INCREMENT,
+  username        TEXT,
+  hashed_password BINARY,
+  salt            BINARY
 )
   COMMENT 'A sample table setup to be used with the login dialog.'
