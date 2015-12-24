4e84ae9f65da612d8fae804480d58a8bd7d97b5f
Caden Sumner <cadenjsumner@gmail.com>
2015-12-24 10:22:08 -0500

    Code Cleanup

--- src/com/doughepi/main/Main.java
+++ src/com/doughepi/main/Main.java
@@ -13,30 +13,27 @@ import javafx.stage.Stage;
 /**
  * The main class, the entry point for this application.
  */
-public class Main extends Application
-{
-	/**
-	 * The main method is only called as a last resort in
-	 * JavaFX applications.
-	 *
-	 * @param args Java program arguments.
-	 */
-	public static void main(String[] args)
-	{
-		launch(args);
-	}
+public class Main extends Application {
+    /**
+     * The main method is only called as a last resort in
+     * JavaFX applications.
+     *
+     * @param args Java program arguments.
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
 
-	/**
-	 * The start method is inherited from Application and is
-	 * called on startup in JavaFX applications.
-	 *
-	 * @param primaryStage The first stage to be displayed.
-	 */
-	@Override
-	public void start(Stage primaryStage)
-	{
-		Manager manager = new Manager(primaryStage);
-		manager.setScene_Login();
-		primaryStage.show();
-	}
+    /**
+     * The start method is inherited from Application and is
+     * called on startup in JavaFX applications.
+     *
+     * @param primaryStage The first stage to be displayed.
+     */
+    @Override
+    public void start(Stage primaryStage) {
+        Manager manager = new Manager(primaryStage);
+        manager.setScene_Login();
+        primaryStage.show();
+    }
 }
--- src/com/doughepi/main/RegisterController.java
+++ src/com/doughepi/main/RegisterController.java
@@ -16,91 +16,75 @@ import javafx.util.Duration;
  *          Several private fields are injected by FXML and represent the ids set for each node in the register.fxml
  *          file.
  */
-public class RegisterController
-{
-	/**
-	 * JavaFX injected fields.
-	 */
-	@FXML
-	private TextField user;
-	@FXML
-	private TextField pass;
-	@FXML
-	private TextField secondPass;
-	@FXML
-	private Button cancelButton;
-	@FXML
-	private Button registerButton;
-	@FXML
-	private Label errorLabel;
+public class RegisterController {
+    /**
+     * JavaFX injected fields.
+     */
+    @FXML
+    private TextField user;
+    @FXML
+    private TextField pass;
+    @FXML
+    private TextField secondPass;
+    @FXML
+    private Button cancelButton;
+    @FXML
+    private Button registerButton;
+    @FXML
+    private Label errorLabel;
 
-	/**
-	 * Initializes event handlers.
-	 *
-	 * @param manager The manager class that controls movement between views.
-	 */
-	public void initializeManager(final Manager manager)
-	{
-		cancelButton.setOnAction(event -> manager.setScene_Login());
+    /**
+     * Initializes event handlers.
+     *
+     * @param manager The manager class that controls movement between views.
+     */
+    public void initializeManager(final Manager manager) {
+        cancelButton.setOnAction(event -> manager.setScene_Login());
 
-		registerButton.setOnAction(event -> registerUser());
-	}
+        registerButton.setOnAction(event -> registerUser());
+    }
 
-	/**
-	 * Invokes the addition of a new user to the database.
-	 */
-	private void registerUser()
-	{
-		String username = user.getText();
-		String password = pass.getText();
-		String secondPassword = secondPass.getText();
+    /**
+     * Invokes the addition of a new user to the database.
+     */
+    private void registerUser() {
+        String username = user.getText();
+        String password = pass.getText();
+        String secondPassword = secondPass.getText();
 
-		Authentication authentication = new Authentication();
+        Authentication authentication = new Authentication();
 
-		try
-		{
-			if (username.isEmpty())
-			{
-				setLabel("Please enter a username.");
-			}
-			else if (password.isEmpty())
-			{
-				setLabel("Please enter a password.");
-			}
-			else if (secondPassword.isEmpty())
-			{
-				setLabel("Please enter the password again.");
-			}
-			else if (!password.equals(secondPassword))
-			{
-				setLabel("Passwords do not match.");
-			}
-			else
-			{
-				authentication.addUser(username, password);
-				setLabel("Success!");
+        try {
+            if (username.isEmpty()) {
+                setLabel("Please enter a username.");
+            } else if (password.isEmpty()) {
+                setLabel("Please enter a password.");
+            } else if (secondPassword.isEmpty()) {
+                setLabel("Please enter the password again.");
+            } else if (!password.equals(secondPassword)) {
+                setLabel("Passwords do not match.");
+            } else {
+                authentication.addUser(username, password);
+                setLabel("Success!");
 
-				cancelButton.setText("Back");
-				registerButton.setDisable(true);
-			}
-		}
-		catch (UsernameTakenException e)
-		{
-			setLabel(e.getMessage());
-		}
-	}
+                cancelButton.setText("Back");
+                registerButton.setDisable(true);
+            }
+        } catch (UsernameTakenException e) {
+            setLabel(e.getMessage());
+        }
+    }
 
-	/**
-	 * Sets the text displayed by the Label errorLabel with and additional fade out effect.
-	 *
-	 * @param text The text to be displayed by the label.
-	 */
-	private void setLabel(String text)
-	{
-		errorLabel.setText(text);
-		FadeTransition fadeTransition = new FadeTransition(Duration.seconds(5), errorLabel);
-		fadeTransition.setFromValue(1.0);
-		fadeTransition.setToValue(0.0);
-		fadeTransition.play();
-	}
+    /**
+     * Sets the text displayed by the Label errorLabel with and additional fade out effect.
+     *
+     * @param text The text to be displayed by the label.
+     */
+    private void setLabel(String text) {
+        errorLabel.setText(text);
+        FadeTransition fadeTransition = new FadeTransition(Duration.seconds(5), errorLabel);
+        fadeTransition.setFromValue(1.0);
+        fadeTransition.setToValue(0.0);
+        fadeTransition.play();
+    }
 }
